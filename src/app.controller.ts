3ac539fb1670939f33e92bc7a90c1a7afbad6a9a
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-08-11 17:17:26 +0300

    Dockerized app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  async healthCheck(): Promise<string> {
+    return 'OK';
   }
 }
